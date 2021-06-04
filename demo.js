e893e2a9e7714e435a58979ea5d5d1815ebde638
anushahs8 <76939656+anushahs8@users.noreply.github.com>
2021-06-04 15:47:09 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,85 @@
+// //STRING
+// let name1 = "anu";
+// console.log(name1);
+
+// // NUMBER
+// let n = 123;
+// n = 12.345;
+// console.log(n);
+
+// //BIGINT
+// const bigInt = 1234567890123456789012345678901234567890n;
+// console.log(bigInt);
+
+// //BOOLEAN
+// let num = 4 > 1;
+// console.log(num);
+
+// let num1 = 1 > 5;
+// console.log(num1);
+
+// //NULL--which represents empty or absence value.
+// var a = null;
+// console.log(a);
+// console.log(typeof (a));
+
+// //UNDEFINED--variable is declared but value is not assigned.
+// var b;
+// console.log(b);  //undefined  
+// console.log(typeof (b));  //undefined 
+
+// //TYPEOF
+// let name2 = "anu";
+// console.log(typeof name2);
+
+
+// let num2 = 123456;
+// console.log(typeof num2);
+
+
+// let value = true;
+// console.log(typeof value);
+
+
+// let name3 = "anusha";
+// console.log(`Hello, ${name3}!`);
+
+// //EXAMPLE
+// add = 5 + 2;
+// console.log(add);
+// sub = 5 - 2;
+// console.log(sub);
+// mul = 5 * 2;
+// console.log(mul);
+// div = 5 / 2;
+// console.log(div);
+
+
+// let value1 = true;
+// console.log('before convert', typeof value1);
+
+// value1 = String;
+// console.log('after convert', typeof value1);
+
+
+// let value2 = false;
+// console.log('before convert', typeof value2);
+
+// value2 = 1123;
+// console.log('after convert', typeof value2);
+
+// let a1 = '45';
+// let b2 = 45;
+// console.log(a == b);
+
+
+let age = 12;
+let result;
+if (age >= 18) {
+      result = "You are eligible to vote.";
+} else {
+      result = "You are not eligible to vote yet.";
+}
+console.log(result);
+
+
